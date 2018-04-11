1113f15ca85424b99495bc94a739e110687f5fe1
David Murray <15840416+dwamurray@users.noreply.github.com>
2018-04-11 16:36:35 +0100

    Update new-info.ps1

--- new-info.ps1
+++ new-info.ps1
@@ -1,7 +1,7 @@
 
 $cipherpath = "HKLM\system\currentcontrolset\control\securityproviders\schannel\ciphers"
 $ciphers = @(
-'null',
+'NULL',
 'DES 56/56',
 'RC2 128/128',
 'RC2 40/128',
@@ -16,6 +16,55 @@ $ciphers = @(
 'Triple DES 168/168'
 )
 
+$protocolpath = "HKLM\system\currentcontrolset\control\securityproviders\schannel\protocols"
+$protocols = @(
+'SSL 2.0\Client',
+'SSL 2.0\Server',
+'SSL 3.0\Client',
+'SSL 3.0\Server',
+'TLS 1.0\Client',
+'TLS 1.0\Server'
+)
+
+$COE = if ( 
+get-content C:\vlogdir\coe08128.log | 
+select-string "9.9.0\\.*has"
+) { 
+"9.9.0 installed"
+} else {
+"9.9.0 not installed"
+}
+
+$KB948963 = if ( 
+get-hotfix | select KB948963
+) {
+"Installed"
+} else {
+"Not installed"
+}
+
+$KB4012598 = if ( 
+get-hotfix | select KB4012598
+) {
+"Installed"
+} else {
+"Not installed"
+}
+
+$obj = new-object –typename psobject
+
+$obj | add-member –membertype noteproperty `
+-name Server –value $env:computername
+
+$obj | add-member –membertype noteproperty `
+-name "Patch level" –value $COE
+
+$obj | add-member –membertype noteproperty `
+-name "KB948963 AES & TLS patch" –value $KB948963
+
+$obj | add-member –membertype noteproperty `
+-name "KB4012598 Crit1 SMB patch" –value $KB4012598
+
 
 foreach ( $cipher in $ciphers ) {
 $value = reg query $cipherpath\$cipher /v Enabled 2>&1
@@ -28,12 +77,41 @@ elseif (
 { $value = "Disabled" }
 else { $value = "Enabled" }
 
-$ciphertable = @{
-Server = $env:computername
-Cipher = $cipher
-Status = $value
+#$ciphertable = @{
+#Server = $env:computername
+#Value = $cipher
+#Status = $value
+#}
+
+#new-object psobject -property $ciphertable
+$obj | add-member –membertype noteproperty `
+-name "$cipher cipher" –value $value
+
 }
 
-new-object psobject -property $ciphertable
+foreach ( $protocol in $protocols ) {
+$value = reg query "$protocolpath\$protocol" /v Enabled 2>&1
+if ( $LASTEXITCODE -eq "1" )
+{ $value = "Not configured" }
+elseif ( 
+( $value | select-string ".x." | select -expand matches | select -expand value) -eq "0x0"`
+-or ( $value | select-string ".x." | select -expand matches | select -expand value) -eq "0"
+)
+{ $value = "Disabled" }
+else { $value = "Enabled" }
+
+#$protocoltable = @{
+#Server = $env:computername
+#Value = $protocol
+#Status = $value
+#}
+
+#new-object psobject -property $protocoltable
+
+$obj | add-member –membertype noteproperty `
+-name "$protocol protocol" –value $value
+
 
 }
+write-output $obj
+

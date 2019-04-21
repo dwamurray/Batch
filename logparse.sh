e19a5fe4c5eda588e92f91ff73cd85d036bef28b
David Murray <15840416+dwamurray@users.noreply.github.com>
2019-04-21 16:16:13 +0100

    Update logparse.sh

--- logparse.sh
+++ logparse.sh
@@ -2,9 +2,14 @@
 # sort them by username & source IP address and count the number of failures for
 # each user
 
+# Sample file: ttps://github.com/dwamurray/repo-depo/blob/master/auth.log
+
 # grep -i "failed password" auth.log | awk -F" " '{print $9}' | uniq -c
 # Gives number of times each user name has been used for an unsuccessful logon attempt
+# Using blank space as delimiting character, the username is the ninth element
+
 # grep -i "failed password" auth.log | awk -F" " '{print $11}' 
-# Gives the source IP address of each failed logon attempt
+# Gives the source IP address of each failed logon attempt, the source IP being the 11th element
 
 grep -i "failed password" auth.log | awk -F" " '{print $9, "", $11}' | uniq -c
+# Prints each unique username and source IP address combination along with a count

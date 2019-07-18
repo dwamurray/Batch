32c6bcf4ea4e2914f013614133a2353d3fd3f217
David Murray <15840416+dwamurray@users.noreply.github.com>
2019-07-18 12:53:26 +0100

    Create portscan.bat

---
+++ portscan.bat
@@ -0,0 +1,16 @@
+REM Need to put portqry.exe into same directory as script OR provide full path to portqry.exe
+REM This checks to see if port 3389 (for RDP) is open on the servers
+REM Good alternative to ping scan, but obviously no match for nmap
+
+@echo off
+if exist portresults.txt (del portresults.txt)
+for /F "delims==" %%i in (servers.txt) do portqry.exe -n %%i -e 3389 -q & if errorlevel 99 ( 
+echo "cannot resolve %%i" >> portresults.txt) else ( 
+if errorlevel 1 ( 
+echo "%%i not listening" >> portresults.txt) else ( 
+if errorlevel 2 ( 
+echo "%%i filtered or offline" >>portresults.txt) else ( 
+echo "%%i accessible" >> portresults.txt 
+ ) 
+  ) 
+   )

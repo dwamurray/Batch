441b8172ef82a8114edfcd4e35d805e7f5e4b116
David Murray <15840416+dwamurray@users.noreply.github.com>
2019-07-18 12:54:12 +0100

    Update portscan.bat

--- portscan.bat
+++ portscan.bat
@@ -1,6 +1,7 @@
 REM Need to put portqry.exe into same directory as script OR provide full path to portqry.exe
 REM This checks to see if port 3389 (for RDP) is open on the servers
 REM Good alternative to ping scan, but obviously no match for nmap
+REM -q switch makes the output of portqry.exe quiet unless an error is generated
 
 @echo off
 if exist portresults.txt (del portresults.txt)

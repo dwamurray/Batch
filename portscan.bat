87d99e0d5eac406a7a08d7b13478e59bb2483faf
David Murray <15840416+dwamurray@users.noreply.github.com>
2019-09-10 09:38:28 +0100

    Update portscan.bat

--- portscan.bat
+++ portscan.bat
@@ -5,13 +5,13 @@ REM -q switch makes the output of portqry.exe quiet unless an error is generated
 
 @echo off
 if exist portresults.txt (del portresults.txt)
-for /F "delims==" %%i in (servers.txt) do portqry.exe -n %%i -e 3389 -q & if errorlevel 99 ( 
+for /F "delims==" %%i in (servers.txt) do portqry.exe -n %%i -e 3389 -q & if errorlevel 99
+( 
 echo "cannot resolve %%i" >> portresults.txt) else ( 
 if errorlevel 1 ( 
-echo "%%i not listening" >> portresults.txt) else ( 
-if errorlevel 2 ( 
-echo "%%i filtered or offline" >>portresults.txt) else ( 
-echo "%%i accessible" >> portresults.txt 
- ) 
-  ) 
-   )
+echo "%%i not listening" >> portresults.txt) else 
+ (if errorlevel 2 
+ (echo "%%i filtered or offline" >>portresults.txt) 
+  else (echo "%%i accessible" >> portresults.txt) 
+ )
+)

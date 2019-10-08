e77d9083b0f375872e7706b399c69f726292857d
David Murray <15840416+dwamurray@users.noreply.github.com>
2019-10-08 15:46:04 +0100

    Update ping.bat

--- ping.bat
+++ ping.bat
@@ -1,7 +1,7 @@
 REM You might get unexpected 'inaccessible' for servers, which might be caused by them returning IPv6 addresses.
 REM Use -4 switch to force use of IPv4 e.g. ping %%i -4 -n 1
 REM -n 1 simply means only ping each server once.
-REM You have to specify errorlevel 1 as the first condition, otherwise hosts that don't resolve will be considered to be 'up'
+REM Specify errorlevel 1 as the first condition, otherwise hosts that don't resolve will be considered to be 'up'
 
 
 @echo off
